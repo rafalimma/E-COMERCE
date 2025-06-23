ef284a168a1d8dc9d7c5171ba648ada01e10265c
rafalimma <rafaelglima79@gmail.com>
2025-06-22 22:50:43 -0300

    adionando allowed hosts

--- project/local_settings.py
+++ project/local_settings.py
@@ -4,7 +4,7 @@ SECRET_KEY = '1!-rlx5(2!e&%4lz)6yh=o(xswhcsgd1^j!+15(p$!$stm#=ip'
 # # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True # motra todos os erros na tela
 
-ALLOWED_HOSTS = ['10.0.2.15']
+ALLOWED_HOSTS = ['10.0.2.15', '0.0.0.0']
 
 DATABASES = {
     'default': {
