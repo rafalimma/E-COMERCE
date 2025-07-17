78b4b72fded4c63b386df0a43eb9858a3cddcdf3
rafalimma <rafaelglima79@gmail.com>
2025-07-17 20:12:38 -0300

    redimensionamento e criando models variações

--- produto/models.py
+++ produto/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from PIL import Image
+import os
+from django.conf import settings
 
 # Create your models here.
 
@@ -15,15 +18,36 @@ class Produto(models.Model):
         ('S', 'Simples')
     ))
 
-def resize_image(img, new_width=800):
-    print(img.name)
+    @staticmethod
+    def resize_image(img, new_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        original_w, original_h = img_pil.size
 
-def save(self, *args, **kwargs):
-    super().save(*args, *kwargs)
+        if original_w <= new_width:
+            img_pil.close()
+            return
+        
+        new_h = round((new_width * original_h) / original_w)
+        new_img = img_pil.resize((new_width, new_h), Image.LANCZOS)
+        new_img.save(img_full_path, optimize=True, quality=50)
 
-    max_image_size = 800
-    if self.imagem:
-        self.resize_image(self.imagem, max_image_size)
+    def save(self, *args, **kwargs):
+        super().save(*args, *kwargs)
 
-def __str__(self):
-    return self.nome
+        max_image_size = 800
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
+    def __str__(self):
+        return self.nome
+    
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
