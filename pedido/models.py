02d06faeec70829497d3d1c18063fb1272113bfb
rafalimma <rafaelglima79@gmail.com>
2025-07-18 16:09:29 -0300

    Criando model Pedidos e item do pedido

--- pedido/models.py
+++ pedido/models.py
@@ -1,3 +1,39 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Pedido(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
+    status = models.CharField(
+        default="Criado",
+        max_length=1,
+        choices=(
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('P', 'Pendente'),
+            ('E', 'Enviado'),
+            ('F', 'finalizado')
+        )
+    )
+    def __str__(self):
+        return f'Pedido N. {self.pk}'
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.CharField(max_length=255)
+    produto_id = models.PositiveIntegerField()
+    variacao = models.CharField(max_length=225)
+    variacao_id = models.PositiveIntegerField()
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    quantidade = models.PositiveIntegerField()
+    imagem = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'Item do {self.pedido}'
+    
+    class Meta:
+        verbose_name = 'Item do pedido'
+        verbose_name_plural = 'Item do pedido'
+
