38a6e516a6866d1f447265400eab84cb5f3b0d24
rafalimma <rafaelglima79@gmail.com>
2025-12-14 23:18:15 -0300

    lidando com variacoes

--- produto/views.py
+++ produto/views.py
@@ -43,6 +43,20 @@ class AdicionarCarrinho(View):
             return redirect(http_referrer)
         
         variacao = get_object_or_404(models.Variacao, id=variacao_id)
+        produto = variacao.produto
+        produto_id = produto.id
+        produto_nome = produto.nome
+        variacao_nome = variacao.nome or ''
+        variacao_id = variacao.id
+        preco_unitario = variacao.preco
+        preco_unitario_promocional = variacao.preco_promocional
+        quantidade = 1
+        imagem = produto.imagem
+
+        if variacao.estoque < 1:
+            messages.error(self.request, 'Estoque insuficiente')
+            return redirect(http_referrer)
+        
         if not self.request.session.get('carrinho'):
             self.request.session['carrinho'] = {}
             self.request.session.save()
@@ -52,8 +66,16 @@ class AdicionarCarrinho(View):
             # TODO Variação existe no carrinho
             pass
         else:
-            # TODO: Variação não existe no carrinho
-            pass
+            carrinho[variacao_id] = {
+                produto_id : produto_id,
+                'produto_nome' : produto_nome,
+                'variacao_nome' : variacao_nome,
+                'variacao_id' : variacao_id,
+                'preco_unitario' : preco_unitario,
+                'preco_unitario_promocional' : preco_unitario_promocional
+                'quantidade' : 1
+                'imagem' : produto_imagem
+            }
 
         return HttpResponse('Adicionar carrinho')
 
